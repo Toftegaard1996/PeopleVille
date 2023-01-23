aa55a5350b481620b6a0bec6582ed95fa52c0463
Andreas Krejberg Deleuran <akd@mb-solutions.dk>
2023-01-23 14:08:11 +0100

    Made inventory system

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Text.Json;
 using PeopleVille;
-
+using PeopleVille.ItemClass;
 
 Console.WriteLine("Welcome to PeopleVille");
 Console.WriteLine("The little simulation village where you can interact, trade and walk around town");
